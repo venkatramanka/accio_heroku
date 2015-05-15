9ee53d91ba651c478e8404f428d23a681f1a2259
venkatramanka <venkatka@outlook.com>
2015-05-15 11:56:02 +0530

    RedisToGo heroku changes

--- config/initializers/chatter_redis.rb
+++ config/initializers/chatter_redis.rb
@@ -3,6 +3,11 @@
 require 'yaml'
 require 'redis'
 config = YAML.load_file('config/chatter_config.yml')
-redis_config = config['redis'][Rails.env]
-$redis = Redis.new(:host => redis_config['host'], :port => redis_config['port'])
+if ENV["REDISTOGO_URL"]
+  uri = URI.parse(ENV["REDISTOGO_URL"])
+  $redis = Redis.new(:host => uri.host, :port => uri.port)
+else
+  redis_config = config['redis'][Rails.env]
+  $redis = Redis.new(:host => redis_config['host'], :port => redis_config['port'])
+end
 $online_users = Array.new
